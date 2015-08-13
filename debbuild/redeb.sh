ef3a3773b8eff4ce59b8082bbf40c8e6422ca54e
Adam Scott <me@adams.co.tt>
2015-08-13 06:37:02 +0000

    Basic application packaging for ubuntu.

---
+++ debbuild/redeb.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/fakeroot bash
+
+# IMPORTANT
+# Protect agaisnt mispelling a var and rm -rf /
+set -u
+set -e
+
+SRC=`mktemp -d`
+DIST=`mktemp -d`
+SYSROOT=${SRC}/sysroot
+APPROOT=${SYSROOT}/opt/piksha
+DEBIAN=${SRC}/DEBIAN
+
+
+cp -r deb-src/* ${SRC}/
+mkdir -p ${APPROOT}
+
+cp ../index.js ${APPROOT}/
+cp ../npm-shrinkwrap.json ${APPROOT}/
+cp ../package.json ${APPROOT}/
+cp ../README.md ${APPROOT}/
+cp ../LICENSE ${APPROOT}/
+
+find ${SRC}/ -type d -exec chmod 0755 {} \;
+find ${SRC}/ -type f -exec chmod go-w {} \;
+chown -R root:root ${SRC}/
+
+let SIZE=`du -s ${SYSROOT} | sed s'/\s\+.*//'`+8
+pushd ${SYSROOT}/
+tar czf ${DIST}/data.tar.gz [a-z]*
+popd
+sed s"/SIZE/${SIZE}/" -i ${DEBIAN}/control
+pushd ${DEBIAN}
+tar czf ${DIST}/control.tar.gz *
+popd
+
+pushd ${DIST}/
+echo 2.0 > ./debian-binary
+
+find ${DIST}/ -type d -exec chmod 0755 {} \;
+find ${DIST}/ -type f -exec chmod go-w {} \;
+chown -R root:root ${DIST}/
+ar r ${DIST}/piksha-1.deb debian-binary control.tar.gz data.tar.gz
+popd
+rsync -a ${DIST}/piksha-1.deb ./
