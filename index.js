0ab04afe2a69affb32b078eded3dc870a1413a7a
Adam Scott <me@adams.co.tt>
2015-08-19 06:38:37 +0000

    Fixing build.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var config = require('/etc/piksha/config')
+var config = require('/etc/piksha/config');
 
 app.get('/', function (req, res) {
   res.send(config.greeting);
