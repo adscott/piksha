418ed76cf3e26c713c6f2f84871c207517f636e4
Adam Scott <me@adams.co.tt>
2015-08-31 11:25:04 +0000

    Added more field types.

--- app/shared/attributes.js
+++ app/shared/attributes.js
@@ -12,10 +12,30 @@ n('piksha.shared', function (ns) {
       name: 'person'
     },
     {
+      name: 'month',
+      error: function (value) {
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return _.contains(months, value) ? '' : 'Input a month, e.g. January';
+      },
+      unique: true
+    },
+    {
       name: 'year',
       error: function (value) {
         return _.inRange(value, 1920, new Date().getFullYear()) ? '' : 'Year must be from 1920 onwards.';
       },
+      unique: true
+    },
+    {
+      name: 'locality',
+      unique: true
+    },
+    {
+      name: 'country',
+      unique: true
+    },
+    {
+      name: 'event',
       unique: true
     }
   ], function (defintion) {
