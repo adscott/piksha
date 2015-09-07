1664a5998acd664e64cfec144ea28770ead13709
Adam Scott <me@adams.co.tt>
2015-09-07 11:15:19 +1000

    Aligning loading spinner.

--- app/client/application/loading.js
+++ app/client/application/loading.js
@@ -1,7 +1,7 @@
 n('piksha.application', function (ns) {
   ns.Loading = React.createClass({
     render: function () {
-      return <div className="loading"><div className="spinner" /></div>;
+      return <div className="routing"><div className="loading"><div className="spinner" /></div></div>;
     }
   });
 });
