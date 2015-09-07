01e84afd98d6c4468706dc8420d4df86270a4c32
Adam Scott <me@adams.co.tt>
2015-09-07 10:52:24 +1000

    Using a CSS spinner.

--- app/client/application/loading.js
+++ app/client/application/loading.js
@@ -1,7 +1,7 @@
 n('piksha.application', function (ns) {
   ns.Loading = React.createClass({
     render: function () {
-      return <img src="/client/assets/img/loader.gif" />;
+      return <div className="loading"><div className="spinner" /></div>;
     }
   });
 });
