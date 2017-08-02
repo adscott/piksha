4f88f4b5cd11d08413f46cae435ff3cf9fed24e9
Adam Scott <me@adams.co.tt>
2017-08-02 11:49:37 +1000

    Migrated vagrant.

--- infra/provision.sh
+++ infra/provision.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 set -e
 
-sudo rm -rf /var/lib/apt/lists/*
+sudo mkdir -p /etc/docker
+sudo cp /vagrant/files/daemon.json /etc/docker
+sudo cp /vagrant/files/subuid /etc/subuid
+sudo cp /vagrant/files/subgid /etc/subgid
+
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install vim nodejs npm nodejs-legacy git memcached
-sudo npm install npm -g
+sudo apt-get -y install docker.io python-pip git
+sudo pip install docker-compose
+
+sudo usermod -aG docker ubuntu
 
 sudo mkdir /etc/piksha
 sudo cp /vagrant/files/config.json /etc/piksha/
 
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
-rm server.pass.key
-openssl req -new -key server.key -out server.csr -subj "/C=AU/ST=NSW/L=Sydney/O=piksha/OU=piksha/CN=piksha"
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-echo '' > server.bundle
+mkdir /tmp/cert_gen
+openssl genrsa -des3 -passout pass:x -out /tmp/cert_gen/server.pass.key 2048
+openssl rsa -passin pass:x -in /tmp/cert_gen/server.pass.key -out /tmp/cert_gen/server.key
+rm /tmp/cert_gen/server.pass.key
+openssl req -new -key /tmp/cert_gen/server.key -out /tmp/cert_gen/server.csr -subj "/C=AU/ST=NSW/L=Sydney/O=piksha/OU=piksha/CN=piksha"
+openssl x509 -req -days 365 -in /tmp/cert_gen/server.csr -signkey /tmp/cert_gen/server.key -out /tmp/cert_gen/server.crt
+echo '' > /tmp/cert_gen/server.bundle
 
-sudo mv /home/vagrant/server.* /etc/ssl/certs/
-
-sudo cp -r /vagrant/files/flickr /opt/flickr
-sudo npm install --prefix /opt/flickr
-sudo cp /vagrant/files/flickr.conf /etc/init/
-sudo start flickr
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org=3.2.4 mongodb-org-server=3.2.4 mongodb-org-shell=3.2.4 mongodb-org-mongos=3.2.4 mongodb-org-tools=3.2.4
+sudo mv /tmp/cert_gen/server.* /etc/ssl/certs/
+rm -rf /tmp/cert_gen
